07394a3fddcf96b2e6d8aba358c45ab8ebb45862
Chinenye <badnenye@gmail.com>
2024-09-22 14:08:27 +0100

    The number buttons are working

--- script.js
+++ script.js
@@ -41,3 +41,56 @@ function operate(numA, numB, operation) {
       result = "ERROR";
   }
 }
+
+let calculatorBtn = document.querySelector(".calculator-btns");
+let calculatorScreen = document.querySelector(".calculator-screen");
+
+calculatorBtn.addEventListener("click", (event) => {
+  let target = event.target;
+
+  switch (target.id) {
+    case "btn-9":
+      calculatorScreen.textContent = 9;
+      break;
+
+    case "btn-8":
+      calculatorScreen.textContent = 8;
+      break;
+
+    case "btn-7":
+      calculatorScreen.textContent = 7;
+      break;
+
+    case "btn-6":
+      calculatorScreen.textContent = 6;
+      break;
+
+    case "btn-5":
+      calculatorScreen.textContent = 5;
+      break;
+
+    case "btn-4":
+      calculatorScreen.textContent = 4;
+      break;
+
+    case "btn-3":
+      calculatorScreen.textContent = 3;
+      break;
+
+    case "btn-2":
+      calculatorScreen.textContent = 2;
+      break;
+
+    case "btn-1":
+      calculatorScreen.textContent = 1;
+      break;
+
+    case "btn-0":
+      calculatorScreen.textContent = 0;
+      break;
+
+    case "btn-.":
+      calculatorScreen.textContent = ".";
+      break;
+  }
+});
