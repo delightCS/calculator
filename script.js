51d40c2c38915d01b708c297081b6ae7b75a4d87
Chinenye <badnenye@gmail.com>
2024-09-24 20:01:13 +0100

    Do some changes. The buttons are still working

--- script.js
+++ script.js
@@ -11,86 +11,40 @@ const multiply = function (numA, numB) {
 };
 
 const divide = function (numA, numB) {
-  if (numB !== 0) {
-    return numA / numB;
-  } else {
-    return "ERROR";
-  }
+  return numA / numB;
 };
 
-let numA = 0;
-let numB = 0;
+let numA = "";
+let numB = "";
 let operation;
 
 function operate(numA, numB, operation) {
   let result;
   switch (operation) {
     case "+":
-      result = add(numA, numB);
+      numA = add(numA, numB);
       break;
     case "-":
-      result = subtract(numA, numB);
+      numA = subtract(numA, numB);
       break;
     case "*":
-      result = multiply(numA, numB);
+      numA = multiply(numA, numB);
       break;
     case "/":
-      result = divide(numA, numB);
+      numA = divide(numA, numB);
       break;
     default:
-      result = "ERROR";
+      numA = "ERROR";
   }
+  return numA;
 }
 
-let calculatorBtn = document.querySelector(".calculator-btns");
-let calculatorScreen = document.querySelector(".calculator-screen");
+const buttons = document.querySelectorAll(".btn");
 
-calculatorBtn.addEventListener("click", (event) => {
-  let target = event.target;
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    numA += this.textContent;
 
-  switch (target.id) {
-    case "btn-9":
-      calculatorScreen.textContent = 9;
-      break;
-
-    case "btn-8":
-      calculatorScreen.textContent = 8;
-      break;
-
-    case "btn-7":
-      calculatorScreen.textContent = 7;
-      break;
-
-    case "btn-6":
-      calculatorScreen.textContent = 6;
-      break;
-
-    case "btn-5":
-      calculatorScreen.textContent = 5;
-      break;
-
-    case "btn-4":
-      calculatorScreen.textContent = 4;
-      break;
-
-    case "btn-3":
-      calculatorScreen.textContent = 3;
-      break;
-
-    case "btn-2":
-      calculatorScreen.textContent = 2;
-      break;
-
-    case "btn-1":
-      calculatorScreen.textContent = 1;
-      break;
-
-    case "btn-0":
-      calculatorScreen.textContent = 0;
-      break;
-
-    case "btn-.":
-      calculatorScreen.textContent = ".";
-      break;
-  }
+    document.querySelector("#calc-screen").textContent = numA;
+  });
 });
